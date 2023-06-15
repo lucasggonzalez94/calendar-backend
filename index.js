0b6aba502d7946fab5dfaaf8e9a7c0cbd001c705
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2023-06-14 22:17:30 -0300

    feat: config variables de entorno y devolver statics

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT;
+
+app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
